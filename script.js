1f5a1e49b76036fc80ffa8f0ffc3b822b3b2634b
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-01-02 12:49:53 -0300

    feat: :construction: Lógica do programa mais completa, mas ainda em desenvolvimento...

--- script.js
+++ script.js
@@ -3,17 +3,38 @@ const randomNumber = Math.round(Math.random() * 9);
 
 alert(randomNumber);
 
+// Variáveis de telas
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+
 let xAttempts = 1;
 
-function getInput() {
+function tryToCheckInput() {
     let input = document.querySelector("#inputNumber");
 
     if(Number(input.value) == randomNumber) {
-        document.querySelector(".screen1").classList.add("hide")
-        document.querySelector(".screen2").classList.remove("hide")
+        screen1.classList.add("hide");
+        screen2.classList.remove("hide");
 
-        document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas!`
-    }
+        document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas!`;
+    };
+    
+    console.log(input.value);
 
-    xAttempts++
+    input.value = "gica do programa "
+
+    xAttempts++;
 }
+
+function resetGame() {
+    screen1.classList.remove("hide");
+    screen2.classList.add("hide");
+
+    xAttempts = 1;
+}
+
+const toTry = document.querySelector("#btnTry");
+const toReset = document.querySelector("#btnReset");
+
+toTry.addEventListener('click', tryToCheckInput)
+toReset.addEventListener('click', )
